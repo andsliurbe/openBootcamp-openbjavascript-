3b59862f545900323b5383b6e8c6c0274f708c6c
andsliurbe <andsliurbebogota@gmail.com>
2022-09-15 06:36:48 -0500

    Ejercicio 2 curso javascript

---
+++ miholamundo/Ejercicio2.js
@@ -0,0 +1,11 @@
+const lista =["Carlos",29,true,new Date(1992,11,26),{titulo:'La Culpa es De La Vaca',autor:"Jaime Lopera",fecha:new Date(2021,10,21),url:"https://g.co/kgs/VMx4su"}]
+
+console.log("Nombre :["+lista[0]+"]");
+console.log("Edad :["+lista[1]+"]");
+console.log("Am I a Dev? :["+lista[2]+"]");
+console.log("Fecha De Nacimieto :["+lista[3]+"]");
+console.log("******************************");
+console.log("Titulo Del Libro Favorito :["+lista[4].titulo+"]");
+console.log("Autor Del Libro Favorito :["+lista[4].autor+"]");
+console.log("Fecha De Publicación Oficial Del Libro Favorito :["+lista[4].fecha+"]");
+console.log("Url Del Libro Favorito :["+lista[4].fecha+"]");
